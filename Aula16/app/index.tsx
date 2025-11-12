09cc70832b5939a91626f6f2f0ba85d992c06981
leehzitha <leticia.burlinskI@gmail.com>
2025-11-12 11:41:37 -0300

    Atualizando telas

--- Aula16/app/index.tsx
+++ Aula16/app/index.tsx
@@ -1,12 +1,33 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { StyleSheet, Text, TextInput, View, TouchableOpacity } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View style={{display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', height: '100%'}}>
-        <View style={{padding: 20, boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)', borderRadius: 20, display: 'flex', gap: 15}}>
+        <View style={{width: '80%', padding: 20,  boxShadow: '0px 6px 100px rgba(0, 0, 0, 0.1)', borderRadius: 20, display: 'flex', gap: 15}}>
             <Text style={styles.title}>Login</Text>
             <Text style={styles.subtitle}>Welcome!</Text>
-            <TextInput style={styles.inputbar} placeholder="User"/>
+            <TextInput style={styles.inputbar} placeholder="Email"/>
+            <TextInput style={styles.inputbar} placeholder="Password" secureTextEntry= {true}/>
+            <TouchableOpacity>
+              <View>
+                <Text style={styles.password}>Forgot Password?</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <View style={{justifyContent: "center", alignItems: "center"}}>
+                <Text style={{color: "white"}}>Login</Text>
+              </View>
+            </TouchableOpacity>
+            <View style={{width: "100%", height: 30, flexDirection: "row", justifyContent: "center", alignItems: "center", gap: "10%", marginTop: "8%",    marginBottom: "8%"}}>
+              <View style={{backgroundColor: "gray", height: "5%", width:"35%"}}></View>
+              <Text style={{color: "gray"}}>OR</Text>
+              <View style={{backgroundColor: "gray", height: "5%", width:"35%"}}></View>
+            </View>
+             <TouchableOpacity style={styles.button}>
+              <View style={{justifyContent: "center", alignItems: "center"}}>
+                <Text style={{color: "white"}}>Create an Account</Text>
+              </View>
+            </TouchableOpacity>
         </View>
 
     </View>
@@ -15,25 +36,14 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
-  colorBlue: {
-    color: "blue",
-    fontSize: 20,
-  },
-
-  square: {
-    height: 300,
-    width: 300,
-    backgroundColor: "pink",
-    flex: 3,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
+  
   title: {
     color: 'orange',
-    fontSize: 22,
+    fontSize: 30,
     fontWeight: 700,
     textAlign: 'center',
+    marginTop: "10%",
+    marginBottom: "10%"
   },
 
   subtitle: {
@@ -44,9 +54,29 @@ const styles = StyleSheet.create({
   },
 
   inputbar: {
-    backgroundColor: '#f2f2f2',
+    backgroundColor: '#f2f5f3',
     padding: 10,
     borderRadius: 10,
+    color: 'gray',
+    width: '100%',
+  },
+
+  password: {
+    color: 'orange',
+    fontSize: 13,
+    textDecorationLine: 'underline',
+    textAlign: 'center',
+
+  },
+
+  button: {
+    color: 'white',
+    backgroundColor: 'orange',
+    borderRadius: 10,
+    textAlign: 'center',
+    padding: 10,
+    width: '100%',
+    fontSize: 20
   }
   
 })
